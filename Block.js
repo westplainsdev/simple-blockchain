3893efc210e884ccabd4422290ec6b7f3f123a08
Chris Berry <chris.berry07@gmail.com>
2019-01-03 15:26:03 -0600

    added proof of work

--- Block.js
+++ Block.js
@@ -7,10 +7,21 @@ module.exports = class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
-}
 
+    //  proof-of-work algorithm
+    mineBlock(difficulty) {
+        // Keep changing the nonce until the hash of our block starts with enough zero's.
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+          this.nonce++;
+          this.hash = this.calculateHash();
+        }
+          
+        console.log("BLOCK MINED: " + this.hash);
+      }
+}
