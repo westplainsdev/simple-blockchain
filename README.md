0002a1d50fc9613d181726cc5d04fa7f6f8ac75b
Chris Berry <chris.berry07@gmail.com>
2019-02-08 08:44:05 -0600

    modified demo flow

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# simple-blockchain
-This is a smiple example of keeping track of changes to an item. 
+# build a simple blockchain with JavaScript
+
+https://www.codementor.io/savjee/how-to-build-a-blockchain-with-javascript-part-1-k7d373dtk
+
+
+https://www.codementor.io/savjee/implementing-proof-of-work-blockchain-in-javascript-part-2-k9ozymkqw
--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const Block = require('./Block');
 const Blockchain = require('./Blockchain');
 
 // Write Javascript code!
-let savjeeCoin = new Blockchain();
-savjeeCoin.addBlock(new Block(1, "12/16/2018", { amount: 4 }));
-savjeeCoin.addBlock(new Block(2, "12/17/2018", { amount: 8 }));
+let chain = new Blockchain();
+console.log('A new block chain has been created: ' + JSON.stringify(chain.getLatestBlock(), null, 4));
+
+// add data to the block chain 
+chain.addBlock(new Block(1, "12/16/2018", { amount: 4 }));
+chain.addBlock(new Block(2, "12/17/2018", { amount: 8 }));
+chain.addBlock(new Block(3, "01/03/2018", { amount: 12 }));
 
 // Check if chain is valid (will return true)
-console.log('Blockchain valid? ' + savjeeCoin.isChainValid());
+console.log('Is Blockchain valid? ' + chain.isChainValid());
 
 // Let's now manipulate the data
-savjeeCoin.chain[1].data = { amount: 100 };
+chain.chain[1].data = { amount: 100 };
+console.log('The data has been manipulated');
 
 // Check our chain again (will now return false)
-console.log("Blockchain valid? " + savjeeCoin.isChainValid());
-console.log("The latest block is: " + JSON.stringify(savjeeCoin.getLatestBlock()));
+console.log("Is Blockchain valid? " + chain.isChainValid());
+
+if(chain.isChainValid()){
+    console.log('The chain currently has %s elements.', chain.getCurrentChainLength());
+    console.log("The latest valid block is: " + JSON.stringify(chain.getLatestBlock(), null, 4));
+   
+} else {
+    console.log('The chain is now invalid');
+    console.log(JSON.stringify(chain.chain[1], null, 4));
+}
+
